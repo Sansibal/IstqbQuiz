19dbc3d2006134d9114cab8c0389fc77477884fb
Gökhan Özcan <goekhanoezcan@freenet.de>
2025-09-08 18:36:22 +0200

    Bugfix using vergessen für APP

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,8 @@ using IstqbQuiz.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
+using IstqbQuiz.Client; // Falls App.razor in diesem Namespace liegt
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
