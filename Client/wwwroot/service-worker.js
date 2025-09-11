676a4acabae20f4734b160e75675dc6d2a2894c3
Gökhan Özcan <goekhanoezcan@freenet.de>
2025-09-11 21:36:14 +0200

    Update und Bugfix für PWA Funktion und Render.com

---
+++ Client/wwwroot/service-worker.js
@@ -0,0 +1,21 @@
+﻿self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open('istqbquiz-cache').then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request).then(networkResponse => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
+        })
+    );
+});
